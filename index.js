821650007839fd1d2a0bb3a21ecf5c29e565fa6d
Kevin Thompson <kevin.thompson@thinkaps.com>
2023-07-20 14:15:16 +0100

    Add morgan logging middleware

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body);
+});
+
 app.use(express.json());
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+// app.use(morgan(':body[tiny]'));
 
 let persons = [
   {
