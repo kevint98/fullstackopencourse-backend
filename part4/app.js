5c73473aaf29bf0e9dcddfbeca00735c5ddf9344
Kevin Thompson <88909437+kevint98@users.noreply.github.com>
2023-09-26 15:45:00 +0100

    Complete exercise 4.15

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -22,5 +24,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
