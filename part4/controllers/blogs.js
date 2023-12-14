762230e500b9ea7b8d3e71a4bdb531b96d0059b5
Kevin Thompson <kevin.thompson@thinkaps.com>
2023-12-14 16:42:19 +0000

    Complete exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,54 +1,63 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (req, res) => {
-	const blogs = await Blog.find({});
-	res.json(blogs);
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
+  res.json(blogs);
 });
 
 blogsRouter.post("/", async (req, res) => {
-	const body = req.body;
+  const body = req.body;
+  const user = await User.findById(body.userId);
 
-	if (!body.likes) {
-		body.likes = 0;
-	}
+  if (!body.likes) {
+    body.likes = 0;
+  }
 
-	if (!body.title || !body.url) {
-		res.status(400).end();
-	} else {
-		const blog = new Blog({
-			title: body.title,
-			author: body.author,
-			url: body.url,
-			likes: body.likes,
-		});
+  if (!body.title || !body.url) {
+    res.status(400).end();
+  } else {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user.id,
+    });
 
-		const savedBlog = await blog.save();
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-		res.status(201).json(savedBlog);
-	}
+    res.status(201).json(savedBlog);
+  }
 });
 
 blogsRouter.delete("/:id", async (req, res) => {
-	await Blog.findByIdAndRemove(req.params.id);
-	res.status(204).end();
+  await Blog.findByIdAndRemove(req.params.id);
+  res.status(204).end();
 });
 
 blogsRouter.put("/:id", async (req, res) => {
-	const body = req.body;
+  const body = req.body;
 
-	const blog = {
-		title: body.title,
-		author: body.author,
-		url: body.url,
-		likes: body.likes,
-	};
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
 
-	const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
-		new: true,
-	});
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+  });
 
-	res.json(updatedBlog);
+  res.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- part4/models/user.js
+++ part4/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, minLength: 3, unique: true },
   name: { type: String },
   passwordHash: { type: String, required: true },
-  blogs: { type: mongoose.Schema.Types.ObjectId, ref: "Blog" },
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }],
 });
 
 userSchema.set("toJSON", {
